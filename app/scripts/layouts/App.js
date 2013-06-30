f05cfb160031adcf80dac021768c5ab55652399c
richterm <richter.maximilian@gmx.net>
2013-06-30 12:35:46 +0200

    adjusted main.js dependencies and marionette specific stuff

---
+++ app/scripts/layouts/App.js
@@ -0,0 +1,11 @@
+define(["backbone.marionette"], function(Marionette) {
+
+	return Marionette.Layout.extend({
+
+		regions: {
+
+		}
+
+	});
+
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,44 @@
 requirejs.config({
 	baseUrl: "scripts",
 	paths: {
+		"backbone.marionette": "../components/backbone.marionette/lib/core/backbone.marionette",
+		"backbone.wreqr": "../components/backbone.marionette/public/javascripts/backbone.wreqr",
+		"backbone.babysitter": "../components/backbone.marionette/public/javascripts/backbone.babysitter",
+		"backbone.augment": "../components/backbone.marionette/public/javascripts/backbone.augment",
 		"underscore": "../components/underscore/underscore",
       "backbone": "../components/backbone/backbone",
       "jquery": "../components/jquery/jquery",
       "require": "../components/requirejs/require",
-      "tpl": "../components/requirejs-tpl/tpl",
-      "router": "routers/Router"
+      "tpl": "../components/requirejs-tpl/tpl"
 	},
 	shim: {
 		"underscore": {
 			deps: [],
 			exports: "_"
+		},
+		"backbone.marionette": {
+			deps: [
+				"backbone",
+				"backbone.wreqr",
+				"backbone.babysitter",
+				"backbone.augment"
+			],
+			exports: "Marionette"
+		},
+		"backbone.babysitter": {
+			deps: [
+				"backbone"
+			]
+		},
+		"backbone.augment": {
+			deps: [
+				"backbone"
+			]
+		},
+		"backbone.wreqr": {
+			deps: [
+				"backbone"
+			]
 		},
 		"backbone": {
 			deps: [
@@ -23,9 +50,25 @@ requirejs.config({
 	}
 });
 
-define(["backbone", "router"], function(Backbone, Router){
+define(["backbone.marionette", "routers/App", "controllers/App", "layouts/App"], function(Marionette, AppRouter, AppController, AppLayout){
 
-	var router = new Router();
-	Backbone.history.start();
+	var App = window.App = new Marionette.Application({});
+
+	App.addInitializer(function(options) {
+		App.appRouter = new AppRouter({
+			controller: new AppController()
+		});
+
+		App.appLayout = new AppLayout({
+			el: "#app"
+		});
+
+	});
+
+	App.on("initialize:after", function(options) {
+		Backbone.history.start();
+	});
+
+	App.start();
 
 });
